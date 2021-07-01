c21f3782ee9c1c8d213e0fec0acdeb74fc0ce821
AbbuTurab <syed.abbuturab@fissionlabs.com>
2021-07-01 18:30:08 +0530

    Removed Client VUe

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,20 +1,3 @@
-// module.exports = {
-//   port: process.env.PORT || 9080,
-//   db: {
-//     database: process.env.DB_NAME || 'Tabtracker',
-//     user: process.env.DB_USER || 'Tabtracker',
-//     password: process.env.DB_PASS || 'Tabtracker',
-//     options: {
-//       dialect: process.env.DIALECT || 'sqlite',
-//       host: process.env.HOST || 'localhost',
-//       storage: './tabtracker.sqlite',
-//     },
-//   },
-//   authentication: {
-//     jwtSecret: process.env.JWT_SECRET || 'secret',
-//   },
-// };
-
 const path = require('path')
 
 module.exports = {
